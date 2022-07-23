34607d1bf433f0e7495b9599997ab13331e4aa51
neeladri22 <neeladri22@gmail.com>
2022-07-23 08:56:01 -0400

    Welcome to the Indian states census analyser Problem

---
+++ IndianStatesCensusAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStatesCensusAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian States Census Analyser Problem");
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+
+using NUnit.Framework;
+using IndianStatesCensusAnalyserProblem;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+
+}
